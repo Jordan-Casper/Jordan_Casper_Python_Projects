de8b976cf1fcc715d8f62551124131b526eb0177
Jordan-Casper <14353592+Jordan-Casper@users.noreply.github.com>
2024-02-17 19:52:25 -0700

    FIrst Commit

---
+++ hello_world/hello_world.py
@@ -0,0 +1,14 @@
+# This program is used to verify that my development environment is setup correct on my current system. 
+# It merely contains basic functionality of Python. 
+import numpy as np
+
+#Print of Hello World.
+print("Hello, World!")
+
+# NumPy array
+array = np.array([5, 3, 2, 4])
+
+#Prints the array
+print(array)
+
+
